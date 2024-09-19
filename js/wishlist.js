f493119f3fb1942f9e53573614834043b1a710cc
AdiJT <aditaklal@gmail.com>
2024-09-19 15:05:12 +0800

    Sedikit

---
+++ js/wishlist.js
@@ -0,0 +1,78 @@
+class WishListItem {
+    constructor(produk, catatan) {
+        this.produk = produk;
+        this.catatan = catatan;
+    }
+}
+
+const wishListSessionKey = "wish-list";
+
+function getWishList() {
+    let wishList;
+    const wishListJson = window.sessionStorage.getItem(wishListSessionKey);
+    if(!wishListJson) {
+        wishList = [];
+    } else {
+        try {
+            wishList = JSON.parse(wishListJson);
+
+            wishList = wishList.map((i) => new WishListItem(
+                new Produk(
+                    i.produk.nama,
+                    i.produk.gambar,
+                    i.produk.harga,
+                    i.produk.hargaDiskon,
+                    i.produk.stok,
+                    i.produk.keterangan,
+                    i.produk.spesifikasi,
+                ),
+                i.catatan
+            ))
+        } catch (error) {
+            wishList = [];
+        }
+    }
+
+    return wishList;
+}
+
+function isInWishList(namaProduk) {
+    const index = wishlist.findIndex((i) => i.produk.nama === namaProduk);
+
+    if (index !== -1)
+        return false;
+
+    return true;
+}
+
+function addItem(produk, catatan) {
+    const wishlist = getWishList();
+
+    if(wishlist.length === 0) {
+        wishlist.push(new WishListItem(produk, catatan));
+    } else {
+        if(!isInWishList(produk.nama))
+            return;
+
+        wishlist.push(new KeranjangItem(produk, catatan));
+    }
+
+    saveWishList(wishlist);
+}
+
+function removeItem(produk) {
+    const wishList = getWishList();
+    const newWishList = wishList.filter((e) => e.produk.nama !== produk.nama);
+
+    saveWishList(newWishList);
+}
+
+function clearWishList() {
+    window.sessionStorage.removeItem(wishListSessionKey);
+    window.location.reload();
+}
+
+function saveWishList(wishList) {
+    window.sessionStorage.setItem(wishListSessionKey, JSON.stringify(wishList));
+    window.location.reload();
+}
