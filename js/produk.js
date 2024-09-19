10e17aed9d33d2da928d24d219c3deca893bfdd8
AdiJT <aditaklal@gmail.com>
2024-09-19 10:20:05 +0800

    Sedikit

--- js/produk.js
+++ js/produk.js
@@ -1,6 +1,7 @@
 class Produk {
-    constructor(nama, gambar, harga, hargaDiskon, stok, keterangan, spesifikasi) {
+    constructor(nama, gambar, kategori, harga, hargaDiskon, stok, keterangan, spesifikasi) {
         this.nama = nama;
+        this.kategori = kategori;
         this.gambar = gambar;
         this.harga = harga;
         this.hargaDiskon = hargaDiskon;
@@ -10,18 +11,30 @@ class Produk {
     }
 }
 
-async function getProducts() {
-    const result = await fetch("/data/produk.json");
-    if(!result.ok) return null;
-    const json = await result.json();
-
-    return json.map((p) => new Produk(
-        p.nama,
-        p.gambar,
-        p.harga,
-        p.hargaDiskon,
-        p.stok,
-        p.keterangan,
-        p.spesifikasi
-    ));
-}
+const daftarProduk = [
+    new Produk(
+        "Se'i Babi Asap",
+        "assets/sei_makanan.png",
+        "makanan",
+        25000,
+        20000,
+        20,
+        "Se'i babi khas NTT",
+        null,
+    ),
+    new Produk(
+        "Tenun Ikat Sumba",
+        "assets/sei_makanan.png",
+        "Kain Tenun",
+        750000,
+        650000,
+        15,
+        "Tenun ikat khas Sumba yang dibuat dengan teknik tradisional dan motif etnik yang unik, cocok sebagai pakaian adat koleksi pribadi",
+        {
+            bahan : "Katun Alami",
+            warna : "Kombinasi merah, hitam, kuning",
+            ukuran : "200 x 60 cm",
+            caraPerawatan : "Cuci dengan air dingin"
+        },
+    ),
+]
