63303fb7223ad1fa44517826e2fb3465b1b1b92c
putra <fernandputra14@gmail.com>
2024-09-26 12:28:56 +0800

    Update dikit

--- js/main.js
+++ js/main.js
@@ -296,3 +296,13 @@ document.addEventListener('click', function(event) {
     filterContainer.forEach(e => e.classList.remove('active'));
   }
 });
+
+window.addEventListener('scroll', function() {
+  const navbar = document.querySelector('nav');
+  
+  if (window.scrollY > 0) {
+    navbar.classList.add('nav--scrolled');
+  } else {
+    navbar.classList.remove('nav--scrolled');
+  }
+});
